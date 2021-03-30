e53c1bde19b536946947bd5b2e1b2ffe5cca23bb
Luiz Guilherme Bandeira <arkanael@gmail.com>
2021-03-29 22:23:21 -0300

    Se o número for negativo fica em vermelho, se for positivo fica em verde e 0 em preto

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,6 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-
 btns.forEach(function(btn){
     btn.addEventListener("click",function(e){
         const styles = e.currentTarget.classList;
@@ -17,6 +16,18 @@ btns.forEach(function(btn){
         else{
             count = 0;
         }
+        
+        if (count > 0)
+        {
+            value.style.color = 'green'
+        }
+        else if(count < 0){
+            value.style.color = 'red'
+        }
+        else{
+            value.style.color = 'black'
+        }
+
         value.textContent = count;
     });
 });
